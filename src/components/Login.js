ac254e3b0e0e8d77eed0b1d223e99a0bf690db61
AKASH RS <akashakash05777@gmail.com>
2025-09-23 20:18:45 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -369,7 +369,7 @@ const Login = () => {
 
         {/* Footer */}
         <div className="text-center mt-6 text-sm text-gray-500">
-          <p>© 2024 SmartCampus. All rights reserved.</p>
+          <p>© 2025 SmartCampus. All rights reserved.</p>
         </div>
       </div>
     </div>
